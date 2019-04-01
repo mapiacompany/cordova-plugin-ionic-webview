283217bb2b35d4942414a0809bbf0cba1b55029e
kyoungheejang <gkdlfl1237@mapiacompany.com>
2019-04-01 14:13:21 +0900

    fix(webView): fix webView for youtube video bug

--- src/android/com/ionicframework/cordova/webview/IonicWebView.java
+++ src/android/com/ionicframework/cordova/webview/IonicWebView.java
@@ -2,10 +2,12 @@ package com.ionicframework.cordova.webview;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import android.webkit.WebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class IonicWebView extends CordovaPlugin  {
+  WebView mWebView;
 
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
   
@@ -23,5 +25,22 @@ public class IonicWebView extends CordovaPlugin  {
     return false;
   }
 
+  @Override
+  public void onResume(boolean multitasking) {
+    super.onResume(multitasking);
+    if(mWebView != null){
+      mWebView.onResume();
+      mWebView.resumeTimers();
+    }
+  }
+
+  @Override
+  public void onPause(boolean multitasking) {
+    super.onPause(multitasking);
+    if(mWebView != null){
+      mWebView.onPause();
+      mWebView.pauseTimers();
+    }
+  }
 }
 
